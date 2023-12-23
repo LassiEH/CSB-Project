b4f1eafe054e38e3a5ad5336afa5fde2b60e73e7
LassiEH <lassi.haarala@live.fi>
2023-12-23 09:36:24 +0200

    Fixes to trying to comment without logging in

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
 from typing import Any
 from django.db.models.query import QuerySet
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
 from django.template import loader
@@ -9,6 +9,7 @@ from django.urls import reverse
 from .models import Question, Choice, Comment
 from .forms import CommentForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 # Create your views here.
 
 class IndexView(generic.ListView):
@@ -31,12 +32,17 @@ class DetailView(generic.DetailView):
     def post(self, request, *args, **kwargs):
         question = self.get_object()
         form = CommentForm(request.POST)
-        if form.is_valid():
-            new_comment = form.save(commit=False)
-            new_comment.user = request.user
-            new_comment.question = question
-            new_comment.save()
-        return self.get(request, *args, **kwargs)
+
+        if request.user.is_authenticated:
+            if form.is_valid():
+                new_comment = form.save(commit=False)
+                new_comment.user = request.user
+                new_comment.question = question
+                new_comment.save()
+            return self.get(request, *args, **kwargs)
+        else:
+            messages.error(request, 'You must log in first to comment!')
+        return redirect('polls:detail', pk = question.pk)
 
 class ResultsView(generic.DetailView):
     model = Question
@@ -63,8 +69,13 @@ def vote(request, question_id):
 def create_comment(request):
     if request.method == 'POST':
         comment_text = request.POST('comment_text')
-        new_comment = Comment.objects.create(user=request.user, comment_text=comment_text)
+        if request.user.is_authenticated:
+            new_comment = Comment.objects.create(user=request.user, comment_text=comment_text)
+            messages.success(request, 'Comment sent.')
+            return render(request, 'polls/detail.html', {'comment': new_comment})
+        else:
+            messages.error(request, 'You must log in first to comment!')
+            return redirect('polls:login')
 
-        return render(request, 'polls/detail.html', {'comment': new_comment})
     else:
         return render(request, 'polls/create_comment.html')
